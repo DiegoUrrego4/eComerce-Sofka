c18ef838d45f5dd3adf18a84a40994d9e1e3ada0
Diego Urrego <diegourrg@gmail.com>
2022-10-24 18:51:27 -0500

    Se crean los componentes <Grid /> y <Card />, se prueba funcionalidad cuando hay varios elementos y se inicia con la implementación del servidor backend.

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -3,17 +3,18 @@ import { CommonModule } from '@angular/common';
 import { StoreComponent } from './pages/store/store.component';
 import { StoreItemComponent } from './pages/store-item/store-item.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-
-
+import { GridComponent } from './components/grid/grid.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     StoreComponent,
     StoreItemComponent,
-    HomePageComponent
+    HomePageComponent,
+    GridComponent,
+    CardComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  exports: [StoreComponent, StoreItemComponent, HomePageComponent],
+  imports: [CommonModule],
 })
-export class ShopModule { }
+export class ShopModule {}
